fc1c271f5c64f755c636e9510e5ee4246887b938
danna <dannadeilydc@ufps.educ.o>
2022-06-10 19:41:33 -0500

    calificar

--- vistas/calificar.php
+++ vistas/calificar.php
@@ -10,44 +10,70 @@ if (!isset($_SESSION['usuario'])||empty($_SESSION['usuario'])|| $_SESSION['rol']
 }
  ?>
 <!DOCTYPE html>
-<html lang="es" dir="ltr">
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/calificar.css">
-    <title>Convocatorias</title>
+    <link rel="stylesheet" href="css/main.css">
+    <title></title>
+    <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   </head>
   <body>
 
+    <main id="main" class="main">
     <header>
       <?php include 'HeaderLogin.php'; ?>
     </header>
+
     <aside class="">
       <?php include 'BarraLateralAdministrador.php' ?>
     </aside>
-<section id="container-calificar">
-  <table id="customers-calificar">
-    <tr>
-      <th>Titulo</th>
-      <th>descripcion</th>
-      <th>Fecha de cierre</th>
-      <th>Dias restantes</th>
-      <th>Acciones</th>
-    </tr>
 
-    <?php for ($i=0; $i <count($listaConvocatorias)-1 ; $i++) {
-      if (date('Y-m-d',strtotime($listaConvocatorias[$i]->fecha_fin.'+ 1 month'))>=date("Y-m-d")) {
-      ?>
-      <tr>
-        <td> <?php echo $listaConvocatorias[$i]->titulo ;?> </td>
-        <td> <?php echo $listaConvocatorias[$i]->descripcion ;?> </td>
-        <td> <?php echo $listaConvocatorias[$i]->fecha_fin;?> </td>
-        <td> <?php echo $convocatoria->diasRestantes($listaConvocatorias[$i]->fecha_fin);?> </td>
-        <td> <abbr title="Calificar"> <a href="calificarCategoria.php?conv=<?php echo $listaConvocatorias[$i]->id_convocatoria; ?>"> <i style="font-size:25px;" class="fas fa-clipboard-check"></i> </a></abbr></td>
-      </tr>
+    <section class="section">
+      <div class="row">
+            <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Listado de convocatorias</h5>
 
-    <?php  }
-   } ?>
-  </table>
+              <!-- Default Table -->
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Titulo Convocatoria</th>
+                    <th scope="col">descripcion</th>
+                    <th scope="col">Fecha de cierre</th>
+                    <th scope="col">Dias restantes</th>
+                    <th scope="col">Seleccionar</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php for ($i=0; $i <count($listaConvocatorias)-1 ; $i++) {
+                    if (date('Y-m-d',strtotime($listaConvocatorias[$i]->fecha_fin.'+ 1 month'))>=date("Y-m-d")) {
+                    ?>
+                    <tr>
+                      <td> <?php echo $listaConvocatorias[$i]->titulo ;?> </td>
+                      <td> <?php echo $listaConvocatorias[$i]->descripcion ;?> </td>
+                      <td> <?php echo $listaConvocatorias[$i]->fecha_fin;?> </td>
+                      <td> <?php echo $convocatoria->diasRestantes($listaConvocatorias[$i]->fecha_fin);?> </td>
+                      <td> <abbr title="Calificar"> <a href="calificarCategoria.php?conv=<?php echo $listaConvocatorias[$i]->id_convocatoria; ?>"> <i style="font-size:25px;" class="fas fa-clipboard-check"></i> </a></abbr></td>
+                    </tr>
+
+                  <?php  }
+                 } ?>
+                </tbody>
+              </table>
+              <!-- End Default Table Example -->
+            </div>
+          </div>
+        </div>
 </section>
+
+</main>
   </body>
 </html>
--- vistas/historial.php
+++ vistas/historial.php
@@ -13,6 +13,7 @@ $count=count($historial);
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/tabla.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Historial</title>
   </head>
   <body>
@@ -24,34 +25,41 @@ $count=count($historial);
       </aside>
 
 
+      <section class="section">
+        <div class="row">
+              <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Historial de convocatorias</h5>
 
-      <section id="container-historial">
-        <legend id="titulo-tabla">Historial de convocatorias</legend>
-
-        <hr>
-        <table id="customers">
-          <tr>
-            <th>Titulo</th>
-            <th>Descripcion</th>
-            <th>Fecha de apertura</th>
-            <th>Fecha de cierre</th>
-            <th>Participantes</th>
-            <th>Ganadores</th>
-          </tr>
-
-        <?php for ($i=0; $i < $count-1; $i++) { ?>
-          <tr>
-            <td> <?php echo $historial[$i]->titulo; ?> </td>
-            <td> <?php echo $historial[$i]->descripcion; ?> </td>
-            <td> <?php echo $historial[$i]->fecha_inicio; ?> </td>
-            <td> <?php echo $historial[$i]->fecha_fin; ?> </td>
-            <td> <a href="participantes.php?conv=<?php echo $historial[$i]->id_convocatoria;  ?>">Participantes</a></td>
-            <td> <a href="ganadores.php?conv=<?php echo $historial[$i]->id_convocatoria;  ?>">Ganadores</a></td>
-          </tr>
-        <?php } ?>
-        </table>
-
-      </section>
-
+                <!-- Default Table -->
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Titulo</th>
+                      <th>Descripcion</th>
+                      <th>Fecha de apertura</th>
+                      <th>Fecha de cierre</th>
+                      <th>Participantes</th>
+                      <th>Ganadores</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php for ($i=0; $i < $count-1; $i++) { ?>
+                      <tr>
+                        <td> <?php echo $historial[$i]->titulo; ?> </td>
+                        <td> <?php echo $historial[$i]->descripcion; ?> </td>
+                        <td> <?php echo $historial[$i]->fecha_inicio; ?> </td>
+                        <td> <?php echo $historial[$i]->fecha_fin; ?> </td>
+                        <td> <a href="participantes.php?conv=<?php echo $historial[$i]->id_convocatoria;  ?>">Participantes</a></td>
+                        <td> <a href="ganadores.php?conv=<?php echo $historial[$i]->id_convocatoria;  ?>">Ganadores</a></td>
+                      </tr>
+                    <?php } ?>
+                  </tbody>
+                </table>
+                <!-- End Default Table Example -->
+              </div>
+            </div>
+          </div>
+    </section>
   </body>
 </html>
